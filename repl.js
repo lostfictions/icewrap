90ef1550785ec4a0025bdc50bcd13a7d16ddb8d7
sa <lostfictions@users.noreply.github.com>
2017-11-22 05:16:43 -0500

    better-behaved repl

--- repl.js
+++ repl.js
@@ -1,23 +1,41 @@
-const repl = require('repl');
-const { PeerioClient } = require('./index');
+const repl = require('repl')
+const readline = require('readline')
+const { PeerioClient } = require('./index')
 
-let [username, accountKey] = process.argv.slice(2, 4);
-console.log(username, accountKey)
+let [username, accountKey] = process.argv.slice(2, 4)
 if(!username || !accountKey) {
-  username = process.env.PEERIO_USERNAME;
-  accountKey = process.env.PEERIO_ACCOUNTKEY;
+  username = process.env.PEERIO_USERNAME
+  accountKey = process.env.PEERIO_ACCOUNTKEY
 }
 
-if (!username || !accountKey) {
-  console.warn(`Missing username or account key. Authentication probably won't succeed.`);
-}
+(async () => {
+  if(!username || !accountKey) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
 
-const client = new PeerioClient('testclient');
-client.login(username, accountKey);
+    username = await new Promise(res => rl.question('Username: ', a => res(a)))
+    accountKey = await new Promise(res => rl.question('Account key: ', a => res(a)))
+    rl.close()
+  }
+  console.log(`Logging in as username '${username}' (with account key of length ${accountKey.length})`)
 
-client.waitForAuth().then(() => {
-  console.log('Auth ok.');
+  const client = new PeerioClient('testclient')
+  client.on('message', data => console.log(data.message.text))
+
+  try {
+    await client.login(username, accountKey)
+    await client.waitForAuth()
+  }
+  catch(e) {
+    console.warn(`[ERROR AUTHENTICATING] ${e}`)
+    process.exit(1)
+  }
+
+  console.log('Auth ok.')
+
   const server = repl.start()
   server.context.client = client
   server.context.ice = client.app
-});
+})()
